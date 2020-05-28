cf2fa70b188807c5b65e0f46acea3c7a15287753
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-05-28 23:09:52 +0900

    Add simple news feed app

---
+++ components/common.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+`;
+
+export const HeadingLg = styled.h1`
+  font-size: 32px;
+  line-height: 1;
+  margin-bottom: 8px;
+`;
+
+export const HeadingMd = styled.h2`
+  font-size: 24px;
+  line-height: 1;
+  margin-bottom: 8px;
+`;
+
+export const Text = styled.p`
+  font-size: 16px;
+  line-height: 1.8;
+  margin-bottom: 8px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,104 @@
-import Head from 'next/head'
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Home() {
+const Index = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div>
+      <Nav>
+        <LogoText>New Release</LogoText>
+      </Nav>
+      <Container>
+        <Card>
+          <CardImageContainer>
+            <Image src="/MinImage.png" alt="" />
+          </CardImageContainer>
+          <CardCategoryBox>
+            <CardCategoryName>Brand Design</CardCategoryName>
+          </CardCategoryBox>
+          <CardTitle>
+            Making the Brand: Asian American and Pacific Islander Heritage Month
+          </CardTitle>
+          <CardParagraph>
+            Meet the creatives and hear the story behind this year’s Asian
+            American and Pacific Islander Heritage Month (AAPIHM) campaign.
+          </CardParagraph>
+        </Card>
+        <Card>
+          <CardImageContainer>
+            <Image src="/MinImage.png" alt="" />
+          </CardImageContainer>
+          <CardCategoryBox>
+            <CardCategoryName>Brand Design</CardCategoryName>
+          </CardCategoryBox>
+          <CardTitle>
+            Making the Brand: Asian American and Pacific Islander Heritage Month
+          </CardTitle>
+          <CardParagraph>
+            Meet the creatives and hear the story behind this year’s Asian
+            American and Pacific Islander Heritage Month (AAPIHM) campaign.
+          </CardParagraph>
+        </Card>
+      </Container>
     </div>
-  )
-}
+  );
+};
+
+const Nav = styled.nav`
+  height: 64;
+  background-color: #fff;
+  padding: 16px 20px;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 24px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Container = styled.div`
+  padding: 0 16px;
+`;
+
+const Card = styled.div`
+  margin-bottom: 24px;
+`;
+
+const CardImageContainer = styled.div`
+  margin-bottom: 24px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 100%;
+`;
+
+const CardCategoryBox = styled.div`
+  background-color: #161313;
+  height: 32px;
+  display: flex;
+  width: 160px;
+  align-items: center;
+  padding: 2px 8px;
+  margin-bottom: 24px;
+`;
+
+const CardCategoryName = styled.h4`
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+`;
+
+const CardTitle = styled.h1`
+  font-size: 26px;
+  color: #161313;
+  line-height: 1.6;
+  margin-bottom: 24px;
+`;
+
+const CardParagraph = styled.p`
+  font-size: 19px;
+  line-height: 1.6;
+`;
+
+export default Index;
